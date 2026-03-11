df6bd23e53cd745f491726e104ed288f2833453c
Juan Flores <florespuchojuan@outlook.com>
2026-03-11 04:24:47 -0500

    Se agrega lo que sear

--- 02.template-bd/mysql/environments/main.tf
+++ 02.template-bd/mysql/environments/main.tf
@@ -29,6 +29,7 @@ module "extraccion" {
   project_id          = var.project_id
   repo_docker_name    = var.repo_docker_name
   job_extraccion_name = var.job_extraccion_name
+  bucket_data_name    = var.bucket_data_name
   sa_email            = module.iam.sa_email # Usamos el correo recién creado
 
   # Variables de conexión a la base de datos
